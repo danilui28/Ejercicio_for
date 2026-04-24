e3ce081aab95e758ca3da6c7279cf12e3941d554
danilui28 <david82.macias@gmail.com>
2026-04-24 16:26:56 -0500

    multiplicacion por 5

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -1,4 +1,12 @@
 function generarTablas(){
-    let contenedor = document.getElementById("container")
-    contenedor.innerHTML ="<h1>PROBANDO</h1>"
+    let contenedor = document.getElementById("tabla-contenedor")
+    let contenido = ""
+    for(let i = 1; i<=10; i++){
+        contenido += "<div class='fila'>";
+        contenido += "<span>" + 5 + " x " + i + "</span>";
+        contenido += "<span> = </span>";
+        contenido += "<span>" + (5 * i) + "</span>";
+        contenido += "</div>";
+    }
+    contenedor.innerHTML = contenido;
 }
